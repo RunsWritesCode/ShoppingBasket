5e20b7a50a83075a17a32795da186dcb1ab6f98d
RunsWritesCode <sophieamullins@gmail.com>
2018-01-19 14:30:59 +0000

    added items to basket test

--- src/main/java/basket/Item.java
+++ src/main/java/basket/Item.java
@@ -2,6 +2,27 @@ package basket;
 
 public class Item {
 
+    public String name;
+    public double price;
 
+    public Item(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
--- src/test/java/BasketTest.java
+++ src/test/java/BasketTest.java
@@ -1,4 +1,23 @@
+import basket.Basket;
+import basket.Item;
+import org.junit.Before;
+
 public class BasketTest {
+
+    Basket basket;
+    Item item;
+    Item item2;
+    Item item3;
+    Item item4;
+
+    @Before
+    public void before() {
+        basket = new Basket();
+        item = new Item("Frog", 4.00);
+        item2 = new Item("Dog", 250.00);
+        item3 = new Item("baguette", 0.99);
+        item4 = new Item("apple", 0.60);
+    }
 
 
 
